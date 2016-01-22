abe7f6e717cc233c7b888ddfe648386381b38fdd
Geremia <>
2016-01-22 00:03:10 -0700

    final implementations

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,7 @@
+#include <QMouseEvent>
+#include <QAction>
+#include <QSettings>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,9 +10,47 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QSettings sts;
+    restoreGeometry(sts.value("MainGeometry").toByteArray());
+    restoreState(sts.value("MainState").toByteArray());
+    ui->woeidEdit->setText(sts.value("ID").toString());
+
+    w = new Weather(this);
+
+    connect(ui->okButton, &QPushButton::clicked, this, &MainWindow::onOkButtonClicked);
+    connect(w, &Weather::updateFinished, this, &MainWindow::onWeatherUpdateFinished);
+    connect(w, &Weather::imageDownloaded, this, &MainWindow::onAttrImgDownloaded);
+    w->getAttrImg();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onOkButtonClicked()
+{
+    w->updateData(ui->woeidEdit->text());
+}
+
+void MainWindow::onAttrImgDownloaded(const QImage &img)
+{
+    ui->attrLabel->setPixmap(QPixmap::fromImage(img));
+}
+
+void MainWindow::onWeatherUpdateFinished(const QString &location, const QString &temp, const QString &wind)
+{
+    ui->locationLabel->setText(location);
+    ui->tempLabel->setText(temp);
+    ui->windLabel->setText(wind);
+}
+
+void MainWindow::closeEvent(QCloseEvent *e) //save prefs
+{
+    QSettings sts;
+    sts.setValue("MainGeometry", saveGeometry());
+    sts.setValue("MainState", saveState());
+    sts.setValue("ID",ui->woeidEdit->text());
+    e->accept();
 }
--- weather.cpp
+++ weather.cpp
@@ -1,5 +1,4 @@
 #include "weather.h"
-#include <QDebug>
 
 Weather::Weather(QObject *parent) : QObject(parent)
 {
@@ -11,25 +10,74 @@ Weather::Weather(QObject *parent) : QObject(parent)
 
 void Weather::updateData(const QString &woeid)
 {
-
+    QUrl url("https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = " + woeid + "&format=json");
+    QNetworkRequest req(url);
+    naManager->get(req);
 }
 
 void Weather::getAttrImg()
 {
-
+    QUrl url("https://poweredby.yahoo.com/purple.png");
+    QNetworkRequest req(url);
+    imgReply = naManager->get(req);
 }
 
 void Weather::onSSLErrors(QNetworkReply *re)
 {
-    QList<QSslError> sslerrors;
-    re->sslErrors(sslerrors);
-    while (sslerrors.size()) {
-        qDebug(sslerrors.front().errorString().data()->toLatin1());
-        sslerrors.pop_front();
-    }
+    //re->ignoreSslErrors();
+    qDebug() << re->errorString();
 }
 
-void Weather::onQueryFinished(QNetworkReply *)
+void Weather::onQueryFinished(QNetworkReply *re)
 {
+    //if ∃ an error
+    if (re->error() != QNetworkReply::NoError) {
+        qDebug() << re->errorString();
+        re->deleteLater();
+        return;
+    }
 
+    //if reply's an image
+    if (re == imgReply) {
+        attrImg = QImage::fromData(imgReply->readAll());
+        emit imageDownloaded(attrImg);
+        imgReply->deleteLater();
+        return;
+    }
+
+    //if reply's JSON
+    QByteArray result = re->readAll();
+    re->deleteLater();
+
+    QJsonParseError err;
+    QJsonDocument doc = QJsonDocument::fromJson(result, &err);
+    if (err.error != QJsonParseError::NoError) { //if ∃ an error
+        qDebug() << err.errorString();
+        return;
+    }
+
+    //otherwise
+    QJsonObject obj = doc.object();
+    QJsonObject res = obj.value("query").toObject().value("results").toObject().value("channel").toObject();
+
+    QJsonObject locObj = res["location"].toObject();
+    QString location;
+    for(QJsonObject::ConstIterator it = locObj.constBegin(); it != locObj.constEnd(); it++) {
+        location.append((*it).toString());
+        if ((it + 1) != locObj.constEnd())
+            location.append(", ");
+    }
+
+    QString temperature = res["item"].toObject()["condition"].toObject()["temp"].toString() + res["units"].toObject()["temperature"].toString();
+
+    QJsonObject windObj = res["wind"].toObject();
+    QString wind;
+    for(QJsonObject::ConstIterator it = windObj.constBegin(); it != windObj.constEnd(); ++it) {
+        wind.append(it.key());
+        wind.append(": ");
+        wind.append((*it).toString());
+        wind.append("\n");
+    }
+
+    emit updateFinished(location, temperature, wind);
 }
